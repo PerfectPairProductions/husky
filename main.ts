4bffcb8f13dbf79d14d2431db868c504627cb6f2
PerfectPairProductions <58237974+PerfectPairProductions@users.noreply.github.com>
2025-10-03 22:12:05 -0400

    setting some text and plans

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (tiles.tileAtLocationIsWall(tiles.getTileLocation(0, 0))) {
+        left_wall_column = 1
+        scene.setBackgroundImage(assets.image`twn_market`)
+        mySprite.setPosition(159, 104)
+        right_wall_column = 0
+    } else {
+        if (tiles.tileAtLocationIsWall(tiles.getTileLocation(0, 0))) {
+            left_wall_column = 1
+            scene.setBackgroundImage(assets.image`twn_market`)
+            mySprite.setPosition(159, 104)
+            right_wall_column = 0
+        } else {
+        	
+        }
+    }
+})
 controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
     pause(10)
     mySprite.setImage(img`
@@ -282,8 +299,10 @@ controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
         `)
     mySprite.x += -3
 })
+let right_wall_column = 0
+let left_wall_column = 0
 let mySprite: Sprite = null
-scene.setBackgroundImage(assets.image`twn_bldg`)
+scene.setBackgroundImage(assets.image`cityscape`)
 mySprite = sprites.create(img`
     . . 4 4 4 . . . . 4 4 4 . . . . 
     . 4 5 5 5 e . . e 5 5 5 4 . . . 
@@ -300,7 +319,9 @@ mySprite = sprites.create(img`
     . . . f 5 f f f 5 f f 5 f . . . 
     . . . f f . . f f . . f f . . . 
     `, SpriteKind.Player)
-mySprite.setPosition(12, 105)
+mySprite.setPosition(12, 104)
 tiles.setWallAt(tiles.getTileLocation(0, 0), true)
 tiles.setWallAt(tiles.getTileLocation(160, 120), true)
-mySprite.setBounceOnWall(true)
+mySprite.setBounceOnWall(false)
+game.setGameOverMessage(true, "Happy Retirement Ray!!")
+game.setGameOverEffect(true, effects.confetti)
